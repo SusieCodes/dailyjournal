0f5a320c43414bffadcae7d9486ecad16e93be53
Susie Stanley <susiestanley00@gmail.com>
2021-09-05 00:48:05 -0500

    updated favicon

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ https://susiecodes.github.io/dailyjournal/
 
 # dailyjournal wireframe
 ![image](https://github.com/SusieCodes/dailyjournal/blob/main/images/dailyjournalwireframe.png)
+
+# use port 3333 to access API
--- scripts/dataManager.js
+++ scripts/dataManager.js
@@ -1,8 +1,4 @@
 export const getPosts = () => {
     return fetch("http://localhost:3333/posts")
     .then(response => response.json())
-    // .then(parsedResponse => {
-    //     // do something with response here
-    //     return parsedResponse;
-    // })
 }
