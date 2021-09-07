2e6a4cf96477df5bec6ab4f5a0e2adb718589de7
Susie Stanley <susiestanley00@gmail.com>
2021-09-06 21:03:55 -0500

    working on getting clicks to return ID of post

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,38 @@ const startJournal = () => {
 }
 
 startJournal();
+
+
+// const postClick = () => {
+//     console.log("You clicked on a journal entry ");
+// }
+
+const applicationElement = document.getElementById("entry--${entryObj.id}");
+
+console.log("appElement value is: ", applicationElement);
+
+applicationElement.addEventListener("click", (event) => {
+    console.log("event is: ", event);
+    console.log("event target id is: ", event.target.id)
+    console.log("event current target id is: ", event.currentTarget.id)
+	if (event.target.id.startsWith("entry")){
+		console.log("post id is: ", event.target.id.split("--")[1])
+	} else if (event.target.id.startsWith("my")) {
+        console.log("post id is: ", event.currentTarget.id.split("-")[1])
+    }
+})
+
+
+// if (event.target.id.startsWith("edit")){
+//     console.log("post clicked to edit", event.target.id.split("--"))
+//     console.log("the id is", event.target.id.split("--")[1])
+
+/* <button id="1" onClick="reply_click(this.id)">B1</button> */
+/* <button id="2" onClick="reply_click(this.id)">B2</button> */
+/* <button id="3" onClick="reply_click(this.id)">B3</button> */
+
+/* <script type="text/javascript">
+function reply_click(clicked_id)
+{
+    alert(clicked_id);
+} */
--- scripts/post.js
+++ scripts/post.js
@@ -5,12 +5,13 @@ export const Post = (entryObj) => {
     return `
         <section id="entry--${entryObj.id}" class="journalEntry">
 
-            <div class="entry-date">${pageDate}</div>
-            <h2 class="entry-concept">${entryObj.concept}</h2>
-            <div class="entry-entry">${entryObj.entry}</div>
-            <div class="mood-style">
+            <div id="my-date" class="entry-date">${pageDate}</div>
+            <h2 id="my-concept" class="entry-concept">${entryObj.concept}</h2>
+            <div id="my-entry" class="entry-entry">${entryObj.entry}</div>
+            <div id="my-mood" class="mood-style">
             <div class="entry-mood">Mood:</div><div class="entry-mood__info">${entryObj.mood}</div>
             </div>
 
         </section>`
   }
+
