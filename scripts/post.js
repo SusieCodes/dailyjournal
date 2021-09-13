2c76fcbfe419a67333fb6546e9395c576d761d4e
Susie Stanley <susiestanley00@gmail.com>
2021-09-12 22:48:58 -0500

    updated dailyjournal.json

--- scripts/post.js
+++ scripts/post.js
@@ -5,11 +5,11 @@ export const Post = (entryObj) => {
     return `
         <section id="entry--${entryObj.id}" class="journalEntry">
 
-            <div id="date" class="entry-date">${pageDate}</div>
-            <h2 id="concept" class="entry-concept">${entryObj.concept}</h2>
-            <div id="entry" class="entry-entry">${entryObj.entry}</div>
-            <div class="mood-style">
-            <div id="mood1" class="entry-mood">Mood:</div><div id="mood2" class="entry-mood__info">${entryObj.mood}</div>
+            <div id="date--${entryObj.id}" class="entry-date">${pageDate}</div>
+            <h2 id="concept--${entryObj.id}" class="entry-concept">${entryObj.concept}</h2>
+            <div id="post--${entryObj.id}" class="entry-entry">${entryObj.entry}</div>
+            <div id="mood--${entryObj.id}" class="mood">
+            <div id="mood1--${entryObj.id}" class="entry-mood">Mood:</div><div id="mood2--${entryObj.id}" class="entry-mood__info">${entryObj.mood}</div>
             </div>
 
         </section>`
